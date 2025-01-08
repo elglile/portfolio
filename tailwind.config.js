fb8884a14fca57ddac86412f172cfcc7e98a8829
elglile <159739109+elglile@users.noreply.github.com>
2025-01-08 11:12:49 +0100

    up

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,31 +1,6 @@
-// /** @type {import('tailwindcss').Config} */
-// export default {
-//   content: [
-//     "./index.html",
-//     "./src/**/*.{js,ts,jsx,tsx}",
-//   ],
-  
-//   theme: {    container: {
-//     center: true,
-//     padding:"2rem",
-//   },
-//     extend: {
-//       colors: {
-//         'regal-blue': '#243c5a',
-//         // 'primary_Color':'bg-gray-200',
-//         'secinde_Color':'bg-gray-100 ',
-//         'Tird_Color': 'red',
-//         'navbar_clr':'#cbd1dd',        
-//       },
-//     },
-//   },
-//   plugins: [
-//     require('tailwind-scrollbar'),
-//   ],
-// }
+import scrollbarHide from 'tailwind-scrollbar-hide';
+import scrollbar from 'tailwind-scrollbar';
 
-
-// tailwind.config.js
 export default {
   content: [
     "./index.html",
@@ -41,18 +16,15 @@ export default {
       colors: {
         'regal-blue': '#243c5a',
         // 'primary_Color':'bg-gray-200',
-        'secinde_Color':'bg-gray-100 ',
+        'secinde_Color': 'bg-gray-100',
         'Tird_Color': '#0077B6',
-        'my-text-color':'black',
-
-        'navbar_clr':'#cbd1dd',    
+        'my-text-color': 'black',
+        'navbar_clr': '#cbd1dd',    
       },
     },
   },
   plugins: [
-      require('tailwind-scrollbar-hide'),
-      require('tailwind-scrollbar'),   
-    ],
-
-}
-  
+    scrollbarHide,
+    scrollbar,   
+  ],
+};
